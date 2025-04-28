9ca1b05e8c63d9291a46da86a70167dd66a8ab7e
annas <anna.surmacz00@gmail.com>
2025-04-28 08:31:17 +0200

    Initial commit - Kalkulator project

---
+++ src/main/java/com/codilla/kalkulator2/Kalkulator2Application.java
@@ -0,0 +1,13 @@
+package com.codilla.kalkulator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kalkulator2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kalkulator2Application.class, args);
+    }
+
+}
